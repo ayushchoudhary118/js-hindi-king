3a2cf04356b733b950783a553dcdc3223538cabd
AYUSH CHOUDHARY <ayshking91@gmail.com>
2024-01-12 19:06:55 +0000

    datatype

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+let name = "ayush"
+let age = 18
+
+// number
+// bigint
+// string
+// boolean
+// null =>standalone value
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof "ayush");
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountid = 14453
+let accountemail = "ahghghj@gmail.com"
+var accountpaswor = "12345"
+accountcity = "jaipur"
+
+console.log(accountid);
+console.table([accountid,accountemail,accountpaswor,accountcity])
+/*
+prefer not to use var
+*/
